4427d1fa75c49130ab02deb3a463ffd64fdf19a5
Nessa <nessa_m00re@yahoo.com>
2017-09-24 02:54:28 +0000

    Add item script

--- script/script.js
+++ script/script.js
@@ -1,1 +1,33 @@
-window.alert('Welcome');
+
+function addItemToList() {
+    // Get name of the item
+    var itemName = $('#new-todo').val();
+
+    // Create a new li for item
+    var checkbox = $('<input>').attr('type', 'checkbox');
+    var itemLi = $('<li>');
+    itemLi.append(checkbox).append(itemName);
+
+    var list = $('.item-list');
+    list.append(itemLi);
+
+    // Clear the input
+    $('#new-todo').val('');
+}
+
+function markItemAsDone(e) {
+    var itemLi = $(e.parent);
+    itemLi.addClass('done');
+}
+
+$(function() {
+    // Listen for form submit
+    $('.add-todo').on('submit', function(e) {
+        e.preventDefault();
+        addItemToList(e);
+    });
+
+    $('input[type="checkbox"]').on('click', function(e) {
+        markItemAsDone(e);
+    });
+});
